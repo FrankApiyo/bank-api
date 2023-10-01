f6fd709a052656e2398ab7a14ccbd84d36a4fd30
apiyo <franklineapiyo@gmail.com>
2023-10-01 18:44:56 +0300

    Add get-and-update account balance utility function

--- src/banks/utils.clj
+++ src/banks/utils.clj
@@ -9,3 +9,19 @@
       slurp
       parse-string
       (get value-key default-value)))
+
+(defn get-and-update-account-balance
+  [context account-id diff]
+  (let [account (get-in context [:request :database account-id])]
+    (update-in account [:ammount] + diff)))
+
+(comment
+  (=
+   {:ammount 300
+    :name "Frankline"}
+   (get-and-update-account-balance
+    {:request
+     {:database
+      {1 {:ammount 200
+          :name "Frankline"}}}}
+    1 100)))
